640f1381bfa64a08efc2689c29cd91bb11be24b4
Calypso Leonard <calypsorleonard@gmail.com>
2022-02-25 12:45:31 -0500

    commit tree 8d40ec76d105fe61b95dd131555865af3b0eb101

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array, match){
+    return array.filter(item => item.toLowerCase() === match.toLowerCase())
+}
+
+function matchName(drivers, match){
+    return drivers.filter(item => item.name === match)
+}
+
+function fuzzyMatch(drivers, string){
+    //const testLength = string.length
+    return drivers.filter(function(item){
+        return item.substring(0, string.length) === string
+    })
+}
+
+//const testArray = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
